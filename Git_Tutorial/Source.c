bd37f56f9fcd841fbad662f09284af4b1ded7589
lucifgy <lucifgy@gmail.com>
2022-03-20 23:24:39 +0300

    Pythagorean Theoremreadme change

--- Git_Tutorial/Source.c
+++ Git_Tutorial/Source.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-
+﻿#include <stdio.h>
+#include <math.h>
 int main()
 {
-	printf("hello world!");
+	float a, b, c, d;
+
+	printf("a + b:");
+	scanf_s("%f %f", &a, &b);
+
+	c = (a * a) + (b * b);
+	d = sqrt(c);
+	
+	printf("Result: %f \n", d);
+
 	return 0;
 }
