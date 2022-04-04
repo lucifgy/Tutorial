94c7404d1e777e7b95241ee1af4b133f432ccebc
lucifgy <lucifgy@gmail.com>
2022-04-04 19:44:16 +0300

    Cleaning

--- Git_Tutorial/Source.c
+++ Git_Tutorial/Source.c
@@ -1,16 +1,6 @@
 ﻿#include <stdio.h>
-#include <math.h>
+
 int main()
 {
-	float a, b, c, d;
-
-	printf("a + b:");
-	scanf_s("%f %f", &a, &b);
-
-	c = (a * a) + (b * b);
-	d = sqrt(c);
-	
-	printf("Result: %f \n", d);
-
 	return 0;
 }
