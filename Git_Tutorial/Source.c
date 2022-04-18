5c8078c6154ca8a89dd7c04441fc55718ff4c2ce
lucifgy <lucifg@gmail.com>
2022-04-18 22:52:57 +0300

    Source update, c-vim_v03

--- Git_Tutorial/Source.c
+++ Git_Tutorial/Source.c
@@ -1,7 +1,20 @@
 ﻿#include <stdio.h>
 
-int main()
+ int main()
 {
-	printf("Hello");
+	int var = 1;
+	int *ptr;
+	
+	ptr = &var;
+
+	printf("Direct access: %d\n", var);
+	printf("Indirect access: %d\n", *ptr);
+	
+	printf("Adress: %d\n", &var);
+	printf("Adress: %d\n", ptr);
+
+	*ptr = 48;
+	printf("Indirect: %d\n", *ptr);
+
 	return 0;
 }
--- Git_Tutorial/c-vim.c
+++ Git_Tutorial/c-vim.c
@@ -12,6 +12,7 @@ int main()
   do {
     printf("1-71(0 exit): ");
     scanf("%s", input);
+		if (strcmp(input, "0") == 0) exit(0);
     strcat(input, ".txt");
 
     FILE *fp;
@@ -23,6 +24,6 @@ int main()
     system(strcat(cmd, input));
 
     fclose(fp);
-  }while (input !=0 ); 
+  }while (1); 
   return 0;
 }
