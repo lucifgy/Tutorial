3a4cdb3732020e5a6351a4041255b82a5691c6f8
lucifgy <lucifg@gmail.com>
2022-05-21 17:32:34 +0300

    Reset

--- Git_Tutorial/Source.c
+++ Git_Tutorial/Source.c
@@ -1,20 +1,7 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
- int main()
+int main()
 {
-	int var = 1;
-	int *ptr;
-	
-	ptr = &var;
-
-	printf("Direct access: %d\n", var);
-	printf("Indirect access: %d\n", *ptr);
-	
-	printf("Adress: %d\n", &var);
-	printf("Adress: %d\n", ptr);
-
-	*ptr = 48;
-	printf("Indirect: %d\n", *ptr);
 
 	return 0;
 }
