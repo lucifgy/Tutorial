713690c3ab0e27b50e51f300289a05895ddc05b1
lucifgy <lucifg@gmail.com>
2022-05-30 14:38:18 +0300

    V4:Alpha ~

--- Git_Tutorial/cvs-int.c
+++ Git_Tutorial/cvs-int.c
@@ -2,6 +2,7 @@
 //An algorithym to find out when the lamp gets turned on
 //(when the graph starts peaking)
 //put the limit before the peak
+//Clean up and Functions
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -44,40 +45,23 @@ int main()
 	fp_line = fopen("line.txt", "w");
 	for(i = 0; i < 2500; i++)
 	{
-		fprintf(fp_line, "%0.9f\t %0.6f\n", row_a[i], row_b[i]);
+		fprintf(fp_line, "%0.9f\t %0.6f\n", row_a[i], row_d[i]);
 		//printf("%f\t %f\t %f\t %f\n", row_a[i], row_b[i], row_c[i], row_d[i]);
 	}
 	fclose(fp_line);
 
-	double sum, sum1, sum2, sum3;
+	double sum;
 
-	for (i = 1; i < 2500; i++)
-	{
-		sum = sum + (row_d[i-1] * (row_a[i] - row_a[i-1])) + ((row_d[i] - row_d[i-1]) * (row_a[i] - row_a[i-1])) * 0.5;
-	}
 	for(i = 1; i < 2500; i++)
 	{
-		sum1 = sum1 + (row_d[i-1] * (row_a[i] - row_a[i-1])) - ((row_d[i] - row_d[i-1]) * (row_a[i] - row_a[i-1])) * 0.5;
+		sum = sum + ((row_d[i] + row_d[i-1]) / (float)2) * (row_a[i] - row_a[i-1]); 
 	}
-	for(i = 1; i < 2500; i++)
-	{
-		sum2 = sum2 + ((row_d[i] + row_d[i-1]) / (float)2) * (row_a[i] - row_a[i-1]); 
-	}
-
-	double h, f;
-	for(i = 1; i < 2499; i++)
-		f = f + row_d[i];
-
-	h = (row_a[2499] - row_a[0]) / 2500;
-	sum3 = 0.5 * h * (row_d[0] + (2 * f) + row_d[2499]);
+	double row_x[] = {6, 5, 4, 5, 7, 1, 5, 8, 6};
 	
-
-
-	printf("Rectangle area + triangle: %0.10f\n", sum);
-	printf("Rectangle area - triangle: %0.10f\n", sum1);
-	printf("Trapezoid area sum:        %0.10f\n", sum2);
-	printf("Trapezoid2 area sum:       %0.10f\n", sum3);
-	//gnuplot("plot 'line.txt' with lines");
+	printf("Lowest array point: %f\n", array_lowest(row_d, sizeof(row_d) / sizeof(row_d[0])));
+	printf("Lowest array point: %f\n", array_highest(row_d, sizeof(row_d) / sizeof(row_d[0])));
+	printf("Trapezoid area sum: %0.10f\n", sum);
+	gnuplot("plot 'line.txt' with lines");
 	remove("line.txt");
 	return 0;
 }
